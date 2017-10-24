b94d38828cca0ab2ef7e3a5e3f3d758b2a85fbf1
ConanZhang <zhangconancomputer@126.com>
2017-10-25 00:28:52 +0800

    调整目录结构

--- spring-boot-learn/src/main/java/com/zkn/springboot/Application.java
+++ spring-boot-learn/src/main/java/com/zkn/springboot/Application.java
@@ -2,12 +2,9 @@ package com.zkn.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 /**
  * Created by zkn on 2017/8/31.
  */
-@EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
