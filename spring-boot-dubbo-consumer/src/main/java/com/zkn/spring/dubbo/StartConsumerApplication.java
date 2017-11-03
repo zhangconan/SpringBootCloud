77ea1750ff9f0f5c2825dd3a026417ee358279f4
ConanZhang <zhangconancomputer@126.com>
2017-11-03 23:16:51 +0800

    新增Dubbo服务消费者模块

--- spring-boot-dubbo-consumer/src/main/java/com/zkn/spring/dubbo/StartConsumerApplication.java
+++ spring-boot-dubbo-consumer/src/main/java/com/zkn/spring/dubbo/StartConsumerApplication.java
@@ -1,19 +1,17 @@
 package com.zkn.spring.dubbo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 /**
- * Created by zkn on 2017/11/2.
+ * Created by zkn on 2017/11/3.
  */
 @SpringBootApplication
-@ImportResource(locations = {"classpath:dubbo/provider/dubbo-provider.xml"})
-public class StartApplication {
+@ImportResource(locations = "classpath:dubbo/consumer/dubbo-consumer.xml")
+public class StartConsumerApplication {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(StartApplication.class, args);
+        SpringApplication.run(StartConsumerApplication.class, args);
     }
 }
---
+++ spring-boot-dubbo-consumer/src/main/java/com/zkn/spring/dubbo/controller/UserInfoController.java
@@ -0,0 +1,27 @@
+package com.zkn.spring.dubbo.controller;
+
+import com.zkn.spring.shared.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zkn on 2017/11/3.
+ */
+@RestController
+@RequestMapping("/userInfo")
+public class UserInfoController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 获取用户信息
+     *
+     * @return
+     */
+    @RequestMapping("/getUserName")
+    public String getUserName() {
+        return userService.getUserName();
+    }
+}
